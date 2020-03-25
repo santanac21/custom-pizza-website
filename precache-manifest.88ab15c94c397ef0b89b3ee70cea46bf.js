9877ba8d2fb4531bd65e101e5c7a49d395e7bd14
santanac21 <csantana.antonio@gmail.com>
2020-03-25 18:35:22 -0400

    Updates

---
+++ precache-manifest.88ab15c94c397ef0b89b3ee70cea46bf.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba9dc76dce6c6bd17913860b83553ef",
+    "url": "/custom-pizza-website/index.html"
+  },
+  {
+    "revision": "b66545e1961a2c96023c",
+    "url": "/custom-pizza-website/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e1ddcf243bd3e31055bb",
+    "url": "/custom-pizza-website/static/css/main.dcf70876.chunk.css"
+  },
+  {
+    "revision": "b66545e1961a2c96023c",
+    "url": "/custom-pizza-website/static/js/2.64c1b6c7.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/custom-pizza-website/static/js/2.64c1b6c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ddcf243bd3e31055bb",
+    "url": "/custom-pizza-website/static/js/main.4712b0cd.chunk.js"
+  },
+  {
+    "revision": "cc04b5e91bb84eb624da",
+    "url": "/custom-pizza-website/static/js/runtime-main.4b0a18a7.js"
+  },
+  {
+    "revision": "f66e0f4c24ed1cb94555d16aca482fba",
+    "url": "/custom-pizza-website/static/media/Thumbs.f66e0f4c.db"
+  },
+  {
+    "revision": "44647c92167a927df543bbbe94cc014d",
+    "url": "/custom-pizza-website/static/media/about-1.44647c92.jpg"
+  },
+  {
+    "revision": "a18b9fe4f63149ad1f32cf02aca80c64",
+    "url": "/custom-pizza-website/static/media/about-2.a18b9fe4.jpg"
+  },
+  {
+    "revision": "5e4a6aadb7b4db50a39f96102b7b1ab8",
+    "url": "/custom-pizza-website/static/media/about-3.5e4a6aad.jpg"
+  },
+  {
+    "revision": "32a37b22f74af7a73db684840b93a185",
+    "url": "/custom-pizza-website/static/media/pizza-car1.32a37b22.jpg"
+  },
+  {
+    "revision": "384b297a76a2bf3a5aaf15ca87156aa5",
+    "url": "/custom-pizza-website/static/media/pizza-car2.384b297a.jpg"
+  },
+  {
+    "revision": "e4e82a0f6e872d31850f07de3fcd27d4",
+    "url": "/custom-pizza-website/static/media/pizza-car3.e4e82a0f.jpg"
+  },
+  {
+    "revision": "5fb2bf3b3764414f4b1e814bc9877036",
+    "url": "/custom-pizza-website/static/media/pizza-jumbo-about.5fb2bf3b.jpg"
+  },
+  {
+    "revision": "9a97a8347d2e961e39e67e7c01b82547",
+    "url": "/custom-pizza-website/static/media/pizza-jumbo-contact.9a97a834.jpg"
+  },
+  {
+    "revision": "4dd0f6ed534e163e63bcf3b61deb8c24",
+    "url": "/custom-pizza-website/static/media/pizza-jumbo-order.4dd0f6ed.jpg"
+  },
+  {
+    "revision": "a1244e2590a8b18e560ffab0fc8faf5c",
+    "url": "/custom-pizza-website/static/media/pizza-jumbo.a1244e25.jpg"
+  },
+  {
+    "revision": "010846419f7485a79d56faa8f81d0333",
+    "url": "/custom-pizza-website/static/media/pizza-order-now.01084641.jpg"
+  },
+  {
+    "revision": "f6feb1f38deadd79cb06d13536caa528",
+    "url": "/custom-pizza-website/static/media/pizza-rewards.f6feb1f3.jpg"
+  },
+  {
+    "revision": "acc85bcf5e95b435153efbe9232bce57",
+    "url": "/custom-pizza-website/static/media/pizza1.acc85bcf.jpg"
+  },
+  {
+    "revision": "f7873dbac9fd708afba8098b17bcc4aa",
+    "url": "/custom-pizza-website/static/media/pizza2.f7873dba.jpg"
+  },
+  {
+    "revision": "d1938ba7bb832becc6e019784ac944a4",
+    "url": "/custom-pizza-website/static/media/pizza3.d1938ba7.jpg"
+  },
+  {
+    "revision": "f90a2ad935bfa2625695c2b49c966b49",
+    "url": "/custom-pizza-website/static/media/pizza4.f90a2ad9.jpg"
+  },
+  {
+    "revision": "2ebb3432266ec0f0e6eb25b80b9972cf",
+    "url": "/custom-pizza-website/static/media/pizza5.2ebb3432.jpg"
+  },
+  {
+    "revision": "da5f84fc1a1b209232c00ccac60912d6",
+    "url": "/custom-pizza-website/static/media/pizza6.da5f84fc.jpg"
+  },
+  {
+    "revision": "31e13a926a6a85f95e6cf64e2b1a6129",
+    "url": "/custom-pizza-website/static/media/pizza_image.31e13a92.jpg"
+  }
+]);
